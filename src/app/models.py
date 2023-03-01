5c5982af667f9fdb10ab9e958e2ef409d6c71336
vagrant <tlauriso@gmail.com>
2023-03-01 20:39:18 +0000

    Start on event backend

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+##Create your models here.
+
+##Day: contains the details of what the calendar needs to operate
+class Event:
+    Start = models.DateTimeField()
+    End = models.DateTimeField()
+    Name = models.CharField(max_length=50)
+    ##Place = models.
