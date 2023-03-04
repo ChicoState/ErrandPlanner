b263a565c1d57aea95f463ef34533b794b8ecb08
vagrant <tlauriso@gmail.com>
2023-03-04 08:34:53 +0000

    Added schedule view. It stores the days of the current week, and stores events in their respective lists.

--- src/app/models.py
+++ src/app/models.py
@@ -2,9 +2,3 @@ from django.db import models
 
 ##Create your models here.
 
-##Day: contains the details of what the calendar needs to operate
-class Event:
-    Start = models.DateTimeField()
-    End = models.DateTimeField()
-    Name = models.CharField(max_length=50)
-    ##Place = models.
