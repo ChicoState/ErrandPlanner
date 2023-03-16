b8e02fa26a9eb74cc9195ed51d006347221f7ec4
Amanda Chambless <amchambless@csuchico.edu>
2023-03-16 11:41:00 -0700

    Added add and edit paths to urls.py #23

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,9 +1,13 @@
 
 from django.urls import include, path
-from app import views as app_views
+# from app import views as app_views
 from . import views
 
 urlpatterns = [
     path('join/', views.join),
     path('login/', views.user_login),
+    path('errands/', views.errands),
+    path('errands/add/', views.addErrand),
+    path('errands/edit/', views.editErrand),
+    path('errands/edit/<int:id>/', views.editErrand)
 ]
