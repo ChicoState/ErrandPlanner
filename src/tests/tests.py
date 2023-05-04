243e55844b52f4c6f161ac8b5ef7faaa96bd18eb
vagrant <tlauriso@gmail.com>
2023-05-04 16:42:28 +0000

    Ran the linter.

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -5,48 +5,44 @@ from pytz import utc
 from app.models import Event
 
 
-
 class ClientTestCase(TestCase):
-    #Smoke Test
+    # Smoke Test
     def test_smoke(self):
         self.assertEqual(1, 1)
-    
-    #Object Function Tests
+
+    # Object Function Tests
     def test_Errand_str(self):
-        user = User.objects.create_user('0', '0')
+        user = User.objects.create_user("0", "0")
         event = Event.objects.create(
-            user = user, 
-            title = 'test', 
-            priority = 1,
-            streetaddr = '1234 nowhere lane',
-            city = 'Chico',
-            state = 'CA',
-            zip = 00000,
-            start = utc.localize(datetime.now()),
-            duration = 5,
-            is_errand = True,
-            scheduled = True
-            )
+            user=user,
+            title="test",
+            priority=1,
+            streetaddr="1234 nowhere lane",
+            city="Chico",
+            state="CA",
+            zip=00000,
+            start=utc.localize(datetime.now()),
+            duration=5,
+            is_errand=True,
+            scheduled=True,
+        )
         data = event.__str__()
-        self.assertEqual(data, "Errand: test (0)" )
+        self.assertEqual(data, "Errand: test (0)")
 
     def test_Event_str(self):
-        user = User.objects.create_user('0', '0')
+        user = User.objects.create_user("0", "0")
         event = Event.objects.create(
-            user = user, 
-            title = 'test', 
-            priority = 1,
-            streetaddr = '1234 nowhere lane',
-            city = 'Chico',
-            state = 'CA',
-            zip = 00000,
-            start = utc.localize(datetime.now()),
-            duration = 5,
-            is_errand = False,
-            scheduled = True
-            )
+            user=user,
+            title="test",
+            priority=1,
+            streetaddr="1234 nowhere lane",
+            city="Chico",
+            state="CA",
+            zip=00000,
+            start=utc.localize(datetime.now()),
+            duration=5,
+            is_errand=False,
+            scheduled=True,
+        )
         data = event.__str__()
-        self.assertEqual(data, "Event: test (0)" )
-
-
-
+        self.assertEqual(data, "Event: test (0)")
