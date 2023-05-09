96d13778f452fd05d81f560c7128cb2831ec1585
Robert Paul <rob.k.paul@gmail.com>
2023-05-09 09:11:20 -0700

    addGCalEvent

--- src/app/utils.py
+++ src/app/utils.py
@@ -4,4 +4,23 @@ def getGCal(request, oauth):
     token = request.session.get("token")
     email = user["email"]
     url = "https://www.googleapis.com/calendar/v3/calendars/%s/events" % email
-    return oauth.google.get(url=url, token=token, request=request).content
+    return oauth.google.get(
+        url=url,
+        token=token,
+        request=request,
+    ).content
+
+
+def addGCalEvent(request, oauth, event):
+    user = request.session.get("user")
+    token = request.session.get("token")
+    email = user["email"]
+    url = "https://www.googleapis.com/calendar/v3/calendars/%s/events/quickAdd" % email
+    return oauth.google.post(
+        url=url,
+        token=token,
+        request=request,
+        json={
+            "text": event,
+        },
+    ).content
